967b71e68d448324bb533b2e23f11b4625619574
Igor Támara <igor@tamarapatino.org>
2012-11-18 22:18:47 -0500

    Added json response

--- colombiahelper/lookco/lookco.py
+++ colombiahelper/lookco/lookco.py
@@ -5,17 +5,28 @@ import psycopg2.extensions
 import logging
 import simplejson
 
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash, jsonify
+from flask import Flask
+from flask import request
+from flask import session
+from flask import g
+from flask import redirect
+from flask import url_for
+from flask import render_template
+from flask import flash
 
 from vectorformats.Feature import Feature
 from vectorformats.Formats import GeoJSON
 
-logging.basicConfig(filename='/tmp/map.log',level=logging.INFO)
+logging.basicConfig(filename='/tmp/map.log', level=logging.INFO)
 
 # configuration
 #DATABASE = 'postgresql://osm:osm@localhost/osm'
-DATABASE="dbname='{0}' user='{1}' host='{2}' password='{3}'".format("osm","osm","localhost","osm")
+DATABASE = "dbname='{0}' user='{1}' host='{2}' password='{3}'".format(
+    "osm",
+    "osm",
+    "localhost",
+    "osm"
+)
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
@@ -26,13 +37,16 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 
+
 def connect_db():
     conn = psycopg2.connect(app.config['DATABASE'])
     return conn.cursor()
 
+
 @app.before_request
 def before_request():
     g.db = connect_db()
+
 
 @app.teardown_request
 def teardown_request(exception):
@@ -42,16 +56,27 @@ def teardown_request(exception):
 @app.route('/')
 def show_entries():
     g.db.execute("SELECT count(*) AS cant,initial AS name FROM ways_to_fix GROUP BY 2  HAVING count(*) < 500 ORDER BY cant DESC,initial")
-    entries = [{'cant': row[0], 'name': row[1].capitalize() if row[1]!=u'' else u'Varios'} for row in g.db.fetchall()]
+    entries = [{'cant': row[0], 'name': row[1].capitalize() if row[1] != u'' else u'Varios'} for row in g.db.fetchall()]
     return render_template('show_entries.html', entries=entries)
+
 
 @app.route('/show/<initial>')
 def show_details(initial):
     q = "SELECT id,name,st_asgeojson(linestring),st_asgeojson(ST_PointN(linestring,ST_NumPoints(linestring)/2)) AS middle FROM ways_to_fix WHERE initial = %s"
-    g.db.execute(q,(initial.upper(),))
-    details = [Feature(f[0],simplejson.loads(f[2]),{'name': f[1], 'middle': simplejson.loads(f[3])}) for f in g.db.fetchall()]
+    g.db.execute(q, (initial.upper(),))
+    details = [Feature(f[0], simplejson.loads(f[2]), {'name': f[1], 'middle': simplejson.loads(f[3])}) for f in g.db.fetchall()]
     geoj = GeoJSON.GeoJSON()
     return geoj.encode(details)
+
+
+@app.route('/hires/')
+def hires():
+    q = "SELECT id, tags, st_asgeojson(linestring) FROM ways WHERE tags ? 'hires';"
+    g.db.execute(q)
+    details = [Feature(f[0], simplejson.loads(f[2]), {'data': f[1]}) for f in g.db.fetchall()]
+    geoj = GeoJSON.GeoJSON()
+    return geoj.encode(details)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -66,6 +91,7 @@ def login():
             flash('You were logged in')
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
+
 
 @app.route('/logout')
 def logout():
