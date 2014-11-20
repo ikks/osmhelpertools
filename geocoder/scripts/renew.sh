124aca39f5176668d5ef211156d3e60427be9596
Igor Támara <igor@tamarapatino.org>
2014-11-20 18:38:11 -0500

    Updated according to geofabrik path

--- geocoder/scripts/renew.sh
+++ geocoder/scripts/renew.sh
@@ -8,17 +8,17 @@ echo "YOU MUST configure localinfo.sh, grab a sample from localinfo.sh.sample"
 exit 0;
 fi;
 
-wget "http://download.geofabrik.de/openstreetmap/south-america/"$COUNTRY".osm.bz2"
-time bunzip2 $COUNTRY".osm.bz2"
+wget "http://download.geofabrik.de/openstreetmap/south-america/"$COUNTRY"-latest.osm.bz2"
+time bunzip2 $COUNTRY"-latest.osm.bz2"
 dropdb $DBNAME
 createdb -O $DBUSER -T template_postgis $DBNAME
-time osm2pgsql -d $DBNAME -l -s -p $DBUSER $COUNTRY".osm"
+time osm2pgsql -d $DBNAME -l -s -p $DBUSER $COUNTRY"-latest.osm"
 psql -c "CREATE EXTENSION hstore;" $DBNAME
 psql -f $OSMOSISPATHSCRIPTS"pgsnapshot_schema_0.6.sql" $DBNAME
 psql -f $OSMOSISPATHSCRIPTS"pgsnapshot_schema_0.6"_linestring.sql $DBNAME
 psql -f $OSMOSISPATHSCRIPTS"pgsnapshot_schema_0.6_bbox.sql" $DBNAME
 psql -f $OSMOSISPATHSCRIPTS"pgsnapshot_schema_0.6_action.sql" $DBNAME
-time $OSMOSIS --read-xml $COUNTRY".osm" --write-pgsql database=$DBNAME user=$DBSUPR host=$DBHOST password=$DBPSUP
+time $OSMOSIS --read-xml $COUNTRY"-latest.osm" --write-pgsql database=$DBNAME user=$DBSUPR host=$DBHOST password=$DBPSUP
 psql -c "GRANT SELECT on nodes, ways, osm_line, osm_nodes, osm_ways, way_nodes to $DBUSER;" $DBNAME
 psql -f /usr/share/postgresql/9.1/extension/kmeans.sql $DBNAME
 psql -c "SELECT *, tags -> 'name' AS name, upper(substr(tags -> 'name',0,(strpos(tags -> 'name',' ')))) AS initial,st_centroid(linestring) as centroid INTO ways_to_fix  FROM ways WHERE tags ?& Array['highway','name'] AND upper(substr(tags -> 'name',0,(strpos(tags -> 'name',' ')+1))) NOT IN ('CALLE ', 'CARRERA ', 'AVENIDA ', 'TRANSVERSAL ', 'DIAGONAL ', 'AUTOPISTA ', 'SALIDA ', 'VÍA ', 'TRONCAL ');CREATE INDEX ON ways_to_fix(id);CREATE INDEX ON ways_to_fix(name);CREATE INDEX ON ways_to_fix(initial);" $DBNAME
@@ -29,7 +29,7 @@ psql -c "GRANT SELECT ON ways_to_fix, inter_to_fix, intersections, places TO $DB
 psql -c "CREATE EXTENSION plpythonu;" $DBNAME
 psql -f reversegeo.sql $DBNAME
 psql -c "DROP DATABASE "$OLDDB";" template1 && psql -c "ALTER DATABASE "$DBNAME" RENAME TO "$OLDDB";" template1
-rm "$COUNTRY".osm
+rm $COUNTRY"-latest.osm"
 redis-cli flushdb
 python load_data.py
 
